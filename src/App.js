ed2a2046ded299a71a07c553d0859e5aca27390c
cikareto <salisa_cct@hotmail.com>
2019-10-05 01:32:31 +0700

    fetching data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect }  from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
+const url = 'https://community-open-weather-map.p.rapidapi.com/forecast?q=Bangkok%2Cth';
+const data = {
+  method: "GET",
+  headers: {
+    "x-rapidapi-key": "88f281211bmsha09ee93c939d6d3p19a0edjsn8b40c077f04b"
+  }
+}
+
+const App = () =>  {
+  const [forecastList, setForecastList] = useState([]);
+
+  useEffect(() => {
+    const getForecastList = async () => {
+      const { data: response } = await axios.get(url, data);
+      setForecastList(response.list);
+    };
+    getForecastList();
+  }, []);
+
+  const renderForecastList = () => {
+    return forecastList.map(forecast => {
+      return (
+        <div key={forecast.dt_txt}>
+          <p className="forecast_txt">${forecast.dt_txt}</p>
+          <p className="forecast_weather">${forecast.weather[0].description}</p>
+        </div>
+      );
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Whether Forecast</h2>
+      {renderForecastList()}
     </div>
   );
 }
